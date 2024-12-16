8eed2d3e2f0e86211c7ca3c70e2fc75ae3da2a3e
eli <john.berengu@student.moringaschool.com>
2024-12-16 16:38:25 +0300

    completed

--- index.js
+++ index.js
@@ -28,21 +28,77 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+// Destructuring assignment of animal sounds
+const { moo, neigh, baa, oink, cluck } = {
+  moo: "cow",
+  neigh: "horse",
+  baa: "sheep",
+  oink: "pig",
+  cluck: "chicken"
+};
+
+
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const {bessie,dolly,babe,little} = {
+  bessie:"cow",
+  dolly:"sheep",
+  babe:"pig",
+  little:"chicken"
+}
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
+const {blackAndWhite,black,pink} = {
+  blackAndWhite:"cow",
+  black:"sheep",
+  pink:"pig"
+}
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
-
+const {red,orange,yellow,green,blue,indigo,violet} = {
+  red:"red",
+  orange:"orange",
+  yellow:"yellow",
+  green:"green",
+  blue:"blue",
+  indigo:"indigo",
+  violet:"violet"
+}
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
+const {r,o,y,g,b,v} = {
+  r:"red",
+  o:"orange",
+  y:"yellow",
+  g:"green",
+  b:"blue",
+  v:"violet"
+}
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
+const {indg} = {
+  indg:"indigo"
+}
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
+const { muppetName, color, song, job, partner } = {
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit"
+};
+
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const { songs: [ , song2, , song4], job: nestedJob, partner: nestedPartner } = {
+  songs: [
+    "Rainbow Connection",
+    "Moving Right Along", 
+    "The Muppet Show Theme", 
+    "I Hope That Something Better Comes Along" 
+  ],
+  job: "Host of The Muppet Show",
+  partner: "Miss Piggy"
+}
